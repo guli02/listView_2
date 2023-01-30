85fd913307660c376bb9e8599350d4855c23aeb2
Elebesova Gulzat <88238675+guli02@users.noreply.github.com>
2023-01-30 22:57:02 +0600

    guli02
    listView2

---
+++ listview2/listview2/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('List View'),
+          centerTitle: true,
+        ),
+        body: BodyListView(),
+        ),
+    );
+  }
+}
+
+class BodyListView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return _myListView();
+  }
+}
+ Widget _myListView(){
+  final List<String> items = List<String>.generate(15, (i) => 'Item $i');
+
+  return ListView.builder(
+    itemCount: items.length,
+    itemBuilder: (context, index){
+      return Card( 
+        child: ListTile(
+          title: Text('${items[index]}'),
+          leading: Icon(Icons.insert_photo, color: Colors.red),
+          trailing: Icon(Icons.keyboard_arrow_right),
+        ),
+      );
+    },
+  );
+ }
